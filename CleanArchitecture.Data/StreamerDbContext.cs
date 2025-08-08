0572288ca69b66deb510c9c4c1dbfe6124794dfd
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-08 14:50:44 -0700

    feat(data): add initial EF Core migration
    - Created initial EF Core migration `20250808214137_Initial`.
    - Added `Streamers` and `Videos` tables with a relationship.
    - Updated `StreamerDbContext` connection string for local setup.
    - Generated model snapshot for migration tracking.

--- CleanArchitecture.Data/StreamerDbContext.cs
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -5,7 +5,7 @@ public class StreamerDbContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(
-            "Server=myServerAddress;Database=myDataBase;User Id=sa;Password=development.2025!;Trusted_Connection=True;");
+            "Server=localhost,1433;Database=Streamer;User Id=sa;Password=development.2025!;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=False");
     }
     
     public DbSet<Streamer> Streamers { get; set; }
