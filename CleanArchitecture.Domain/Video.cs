f5de42569667adf20cf59da5efbe0d8c86cc1abf
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-08 12:33:45 -0700

    Update project structure and add new domain classes
    - Enhance `.gitignore` to exclude additional files and directories.
    - Update `CleanArchitecture.Data.csproj` and `CleanArchitecture.Domain.csproj` to target .NET 8.0 with implicit usings and nullable reference types.
    - Add `Streamer` and `Video` classes with relevant properties.
    - Modify `CleanArchitecture_VaxiDrez.sln` to include new projects.
    - Replace existing "Hello, World!" line in `Program.cs` for consistency.

---
+++ CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,7 @@
+﻿namespace CleanArchitecture.Domain;
+public class Video
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public virtual Streamer? Streamer { get; set; }
+}
