f82653e04a4a0f879806ef6e153b82bc2d6c9d70
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-08 14:34:33 -0700

    feat(data): add EF Core setup and initial DbContext
    - Added `Microsoft.EntityFrameworkCore.SqlServer` package to `CleanArchitecture.Data.csproj`.
    - Included project reference to `CleanArchitecture.Domain`.
    - Created `StreamerDbContext` with `Streamers` and `Videos` DbSets.
    - Configured SQL Server connection string in `StreamerDbContext`.
    - Introduced `GlobalUsings.cs` to simplify namespace usage.

---
+++ CleanArchitecture.Data/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿// Global using directives
+
+global using CleanArchitecture.Domain;
+global using Microsoft.EntityFrameworkCore;
---
+++ CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchitecture.Data;
+
+public class StreamerDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=myServerAddress;Database=myDataBase;User Id=sa;Password=development.2025!;Trusted_Connection=True;");
+    }
+    
+    public DbSet<Streamer> Streamers { get; set; }
+    public DbSet<Video> Videos { get; set; }
+}
