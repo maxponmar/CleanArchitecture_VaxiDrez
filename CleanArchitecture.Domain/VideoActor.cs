2d96190402561f9a4913a829566e0b8140b7fc5f
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-11 16:46:15 -0700

    feat(domain): add ValueObject base class and enhance domain model
    - Introduced `ValueObject` abstract base class to encapsulate value equality logic.
    - Implemented `EqualOperator`, `NotEqualOperator`, and `GetEqualityComponents` methods for consistent comparisons.
    - Updated `VideoActor` to inherit from `BaseDomainModel` for consistency across domain entities.

--- CleanArchitecture.Domain/VideoActor.cs
+++ CleanArchitecture.Domain/VideoActor.cs
@@ -1,6 +1,6 @@
 ﻿namespace CleanArchitecture.Domain;
 
-public class VideoActor
+public class VideoActor : BaseDomainModel
 {
     public int VideoId { get; set; }
     public int ActorId { get; set; }
