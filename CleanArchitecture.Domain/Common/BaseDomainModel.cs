f0196b36bcccf8a91b579fe6a78a0c1c94a23c28
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-11 16:33:08 -0700

    feat(domain): enhance BaseDomainModel with audit fields
    - Marked `BaseDomainModel` as abstract for better extensibility.
    - Added audit fields: `CreatedAt`, `UpdatedAt`, `CreatedBy`, and `UpdatedBy` for tracking changes.

--- CleanArchitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace CleanArchitecture.Domain.Common;
 
-public class BaseDomainModel
+public abstract class BaseDomainModel
 {
     public int Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public int? CreatedBy { get; set; }
+    public int? UpdatedBy { get; set; }
 }
