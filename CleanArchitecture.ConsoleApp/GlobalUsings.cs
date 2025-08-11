4682bd5b04d7bfb7e1462bb07ab667d1c9461fb5
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-11 13:45:33 -0700

    chore(project): remove console app and update solution structure
    - Deleted `CleanArchitecture.ConsoleApp` project and related files.
    - Removed console app references from the solution file.
    - Organized solution structure with new folders: `src`, `doc`, `test`, `API`, `Core`, and `Infraestructure`.
    - Updated EF Core migration to handle new relationships and constraints.

--- CleanArchitecture.ConsoleApp/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿// Global using directives
-
-global using CleanArchitecture.Data;
-global using CleanArchitecture.Domain;
-global using Microsoft.EntityFrameworkCore;
--- CleanArchitecture.Data/Entities/20250811191852_agregar-tablas-entidades-relaciones.Designer.cs
+++ CleanArchitecture.Data/Entities/20250811191852_agregar-tablas-entidades-relaciones.Designer.cs
@@ -3,6 +3,7 @@ using CleanArchitecture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CleanArchitecture.Data.Entities
 {
     [DbContext(typeof(StreamerDbContext))]
-    partial class StreamerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250811191852_agregar-tablas-entidades-relaciones")]
+    partial class agregartablasentidadesrelaciones
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
