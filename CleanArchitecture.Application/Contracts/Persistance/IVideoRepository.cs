10b702bb92f3f6d35e0bc30010b9416a190c3579
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-14 07:07:16 -0700

    feat(application): extend IVideoRepository with username query
    - Added `GetVideoByUsername` method to `IVideoRepository` for querying videos by username.

--- CleanArchitecture.Application/Contracts/Persistance/IVideoRepository.cs
+++ CleanArchitecture.Application/Contracts/Persistance/IVideoRepository.cs
@@ -3,4 +3,5 @@
 public interface IVideoRepository : IAsyncRepository<Video>
 {
     Task<IEnumerable<Video>> GetVideoByName(string name);
+    Task<IEnumerable<Video>> GetVideoByUsername(string username);
 }
