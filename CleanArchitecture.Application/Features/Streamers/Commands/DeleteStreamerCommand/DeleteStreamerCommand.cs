d0640ba0bf13a3ccf23e240be4de35aedd420ecd
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-15 07:19:01 -0700

    feat(application): add DeleteStreamer command and handler
    - Implemented `DeleteStreamerCommand` and `DeleteStreamerCommandHandler` for removing streamers by ID.
    - Integrated `IStreamerRepository` for repository operations and `ILogger` for logging.
    - Updated folder structure to include dedicated `DeleteStreamerCommand` feature.

---
+++ CleanArchitecture.Application/Features/Streamers/Commands/DeleteStreamerCommand/DeleteStreamerCommand.cs
@@ -0,0 +1,6 @@
+﻿namespace CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamerCommand;
+
+public class DeleteStreamerCommand
+{
+    public int Id { get; set; }
+}
---
+++ CleanArchitecture.Application/Features/Streamers/Commands/DeleteStreamerCommand/DeleteStreamerCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArchitecture.Application.Features.Streamers.Commands.DeleteStreamerCommand;
+
+public class DeleteStreamerCommandHandler(
+    IStreamerRepository streamerRepository, 
+    ILogger<DeleteStreamerCommandHandler> logger)
+{
+    public async Task Handle(DeleteStreamerCommand request, CancellationToken cancellationToken)
+    {
+        var streamerToDelete = await streamerRepository.GetByIdAsync(request.Id);
+        await streamerRepository.DeleteAsync(streamerToDelete);
+        logger.LogInformation($"Streamer with id: {request.Id} deleted successfully.");
+    }
+}
