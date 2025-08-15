a448d00e88a249c3cbce80eae2b247afb4a24ddc
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-15 09:03:18 -0700

    feat(application): add UnhandledExceptionMiddleware for error logging
    - Implemented `UnhandledExceptionMiddleware` to catch and log exceptions during request processing.
    - Logs include exception details, message name, and message content for better traceability.
    - Ensures unhandled exceptions are gracefully logged before being rethrown.

---
+++ CleanArchitecture.Application/Middlewares/UnhandledExceptionBehaviour.cs
@@ -0,0 +1,25 @@
+﻿namespace CleanArchitecture.Application.Middlewares;
+
+public class UnhandledExceptionMiddleware(ILogger<UnhandledExceptionMiddleware> logger)
+{
+    private readonly ILogger _logger = logger;
+
+    public async ValueTask<object?> HandleAsync(object message, Func<object, ValueTask<object?>> inner)
+    {
+        try
+        {
+            return await inner(message);
+        }
+        catch (Exception ex)
+        {
+            var messageName = message.GetType().Name;
+            
+            _logger.LogError(ex, 
+                "Unhandled Exception: An exception occurred while processing request {MessageName}. Message details: {@Message}", 
+                messageName, 
+                message);
+
+            throw;
+        }
+    }
+}
