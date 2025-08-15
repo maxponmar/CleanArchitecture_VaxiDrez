499d8d8c1fa7b0ca187ad79c0e631e9e1e8cf3ee
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-15 08:53:31 -0700

    feat(application): add ValidationMiddleware for request validation
    - Implemented `ValidationMiddleware` to intercept messages and validate using associated validators.
    - Integrated request-based validation with FluentValidation.
    - Throws `ValidationException` for invalid requests to ensure data integrity.

---
+++ CleanArchitecture.Application/Middlewares/ValidationMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanArchitecture.Application.Middlewares;
+
+public class ValidationMiddleware(IServiceProvider serviceProvider)
+{
+    public async ValueTask<object?> HandleAsync(object message, Func<object, ValueTask<object?>> inner)
+    {
+        var validatorType = typeof(IValidator<>).MakeGenericType(message.GetType());
+
+        if (serviceProvider.GetService(validatorType) is not IValidator validator) return await inner(message);
+        
+        var contextType = typeof(ValidationContext<>).MakeGenericType(message.GetType());
+        var validationContext = Activator.CreateInstance(contextType, message);
+
+        var validateMethod = validator.GetType().GetMethod("Validate", [contextType]);
+
+        if (validateMethod?.Invoke(validator, [validationContext]) is ValidationResult { IsValid: false } result)
+            throw new ValidationException(result.Errors);
+
+        return await inner(message);
+    }
+}
