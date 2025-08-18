ea6991e387f595b7c339da72967d91b3d2872b8b
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-18 08:23:28 -0700

    feat(api): add VideoController with GetVideosByUsername endpoint
    - Implemented `VideoController` with a `GetVideosByUsername` action.
    - Integrated `IMessageBus` to handle query execution for retrieving videos.
    - Added `GlobalUsings.cs` to include necessary dependencies.
    - Updated project file to reflect new additions.

---
+++ CleanArchitecture.API/Controllers/VideoController.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanArchitecture.API.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class VideoController(IMessageBus messageBus) : ControllerBase
+{
+    [HttpGet("{username}", Name = "GetVideosByUsername")]
+    [ProducesResponseType(typeof(IEnumerable<VideosVm>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<IEnumerable<VideosVm>>> GetVideosByUsername(string username)
+    {
+        var getUserVideosQuery = new GetVideosListQuery(username);
+        var videos = await messageBus.InvokeAsync<IEnumerable<VideosVm>>(getUserVideosQuery);
+    
+        return Ok(videos);
+    }
+}
---
+++ CleanArchitecture.API/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿// Global using directives
+
+global using CleanArchitecture.Application.Features.Videos.Queries.GetVideosList;
+global using Microsoft.AspNetCore.Mvc;
+global using Wolverine;
