b88193962d945c7760c5d7a4e654a8b204deac5e
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-19 08:34:02 -0700

    feat(infrastructure): add EmailService implementation
    - Implemented `EmailService` class to send emails using SendGrid.
    - Added dependency injection for `IOptions<EmailSettings>` and `ILogger<EmailService>`.
    - Updated `GlobalUsings.cs` with required SendGrid and email-related namespaces.
    - Refactored project structure to include `Email` namespace.

---
+++ CleanArchitecture.Infraestructure/Email/EmailService.cs
@@ -0,0 +1,26 @@
+﻿namespace CleanArchitecture.Infraestructure.Email;
+
+public class EmailService(IOptions<EmailSettings> emailSettings, ILogger<EmailService> logger) : IEmailService
+{
+    public async Task<bool> SendEmailAsync(Application.Models.Email email)
+    {
+        var client = new SendGridClient(emailSettings.Value.ApiKey);
+
+        var subject = email.Subject;
+        var to = new EmailAddress(email.To);
+        var emailBody = email.Body;
+
+        var from = new EmailAddress(emailSettings.Value.FromAddress, emailSettings.Value.FromName);
+
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+
+        var response = await client.SendEmailAsync(msg);
+
+        if(response.StatusCode == System.Net.HttpStatusCode.Accepted || 
+               response.StatusCode == System.Net.HttpStatusCode.OK)
+            return true;
+        
+        logger.LogError($"Error sending email to {email.To}");
+        return false;
+    }
+}
--- CleanArchitecture.Infraestructure/GlobalUsings.cs
+++ CleanArchitecture.Infraestructure/GlobalUsings.cs
@@ -1,9 +1,14 @@
 ﻿// Global using directives
 
 global using System.Linq.Expressions;
+global using CleanArchitecture.Application.Contracts.Infraestructure;
 global using CleanArchitecture.Application.Contracts.Persistance;
+global using CleanArchitecture.Application.Models;
 global using CleanArchitecture.Domain;
 global using CleanArchitecture.Domain.Common;
 global using CleanArchitecture.Infraestructure.Persistance;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using SendGrid;
+global using SendGrid.Helpers.Mail;
