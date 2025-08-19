23a4034fdf3491b0a696e698f7f5efb632f17140
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-19 07:05:38 -0700

    feat(infrastructure): add data seeding for StreamerDbContext
    - Implemented `StreamerDbContextSeed` class to seed preconfigured streamer data.
    - Added `SeedAsync` method to populate `Streamers` table if empty.
    - Included preconfigured streamers: Twitch, Netflix, and Amazon Prime.

---
+++ CleanArchitecture.Infraestructure/Persistance/StreamerDbContextSeed.cs
@@ -0,0 +1,24 @@
+﻿namespace CleanArchitecture.Infraestructure.Persistance;
+
+public class StreamerDbContextSeed
+{
+    public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContextSeed> logger)
+    {
+        if (!context.Streamers.Any())
+        {
+            await context.Streamers.AddRangeAsync(GetPreconfiguredStreamers());
+            await context.SaveChangesAsync();
+            logger.LogInformation("Seed database associated with context {DbContextName}", typeof(StreamerDbContext).Name);
+        }
+    }
+
+    private static IEnumerable<Streamer> GetPreconfiguredStreamers()
+    {
+        return new List<Streamer>()
+        {
+            new Streamer { CreatedBy = "maxponce", Nombre = "Twitch", Url = "https://www.twitch.tv" },
+            new Streamer { CreatedBy = "maxponce", Nombre = "Netflix", Url = "https://www.netflix.com/" },
+            new Streamer { CreatedBy = "maxponce", Nombre = "Amazon Prime", Url = "https://www.amazonprime.com/" },
+        };
+    }
+}
