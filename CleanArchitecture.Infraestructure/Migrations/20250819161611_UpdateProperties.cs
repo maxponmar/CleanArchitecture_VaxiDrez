50ca817ba16e44023999ed1bff77c5cbfe5074dd
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-19 09:35:42 -0700

    feat(infrastructure): update entity properties and rename tables
    - Renamed tables to pluralized format for consistency.
    - Modified properties to enforce indexed and unique constraints.
    - Added new indexes for key entity properties.
    - Updated cascade delete behavior for foreign keys.

---
+++ CleanArchitecture.Infraestructure/Migrations/20250819161611_UpdateProperties.cs
@@ -0,0 +1,334 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CleanArchitecture.Infraestructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Director_Videos_VideoId",
+                table: "Director");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VideoActor_Actor_ActorId",
+                table: "VideoActor");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VideoActor_Videos_VideoId",
+                table: "VideoActor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VideoActor",
+                table: "VideoActor");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Director",
+                table: "Director");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actor",
+                table: "Actor");
+
+            migrationBuilder.RenameTable(
+                name: "VideoActor",
+                newName: "VideoActors");
+
+            migrationBuilder.RenameTable(
+                name: "Director",
+                newName: "Directors");
+
+            migrationBuilder.RenameTable(
+                name: "Actor",
+                newName: "Actors");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VideoActor_ActorId",
+                table: "VideoActors",
+                newName: "IX_VideoActors_ActorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Director_VideoId",
+                table: "Directors",
+                newName: "IX_Directors_VideoId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Videos",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Streamers",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Directors",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Directors",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Actors",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Actors",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VideoActors",
+                table: "VideoActors",
+                columns: new[] { "VideoId", "ActorId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Directors",
+                table: "Directors",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actors",
+                table: "Actors",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Video_Nombre",
+                table: "Videos",
+                column: "Nombre");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Streamer_Nombre_Unique",
+                table: "Streamers",
+                column: "Nombre",
+                unique: true,
+                filter: "[Nombre] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Director_Nombre_Apellido",
+                table: "Directors",
+                columns: new[] { "Nombre", "Apellido" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actor_Nombre_Apellido",
+                table: "Actors",
+                columns: new[] { "Nombre", "Apellido" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Directors_Videos_VideoId",
+                table: "Directors",
+                column: "VideoId",
+                principalTable: "Videos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VideoActors_Actors_ActorId",
+                table: "VideoActors",
+                column: "ActorId",
+                principalTable: "Actors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VideoActors_Videos_VideoId",
+                table: "VideoActors",
+                column: "VideoId",
+                principalTable: "Videos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Directors_Videos_VideoId",
+                table: "Directors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VideoActors_Actors_ActorId",
+                table: "VideoActors");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_VideoActors_Videos_VideoId",
+                table: "VideoActors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Video_Nombre",
+                table: "Videos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Streamer_Nombre_Unique",
+                table: "Streamers");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_VideoActors",
+                table: "VideoActors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Directors",
+                table: "Directors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Director_Nombre_Apellido",
+                table: "Directors");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Actors",
+                table: "Actors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Actor_Nombre_Apellido",
+                table: "Actors");
+
+            migrationBuilder.RenameTable(
+                name: "VideoActors",
+                newName: "VideoActor");
+
+            migrationBuilder.RenameTable(
+                name: "Directors",
+                newName: "Director");
+
+            migrationBuilder.RenameTable(
+                name: "Actors",
+                newName: "Actor");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_VideoActors_ActorId",
+                table: "VideoActor",
+                newName: "IX_VideoActor_ActorId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Directors_VideoId",
+                table: "Director",
+                newName: "IX_Director_VideoId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Videos",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Streamers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Director",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Director",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Actor",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Apellido",
+                table: "Actor",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_VideoActor",
+                table: "VideoActor",
+                columns: new[] { "VideoId", "ActorId" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Director",
+                table: "Director",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Actor",
+                table: "Actor",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Director_Videos_VideoId",
+                table: "Director",
+                column: "VideoId",
+                principalTable: "Videos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VideoActor_Actor_ActorId",
+                table: "VideoActor",
+                column: "ActorId",
+                principalTable: "Actor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VideoActor_Videos_VideoId",
+                table: "VideoActor",
+                column: "VideoId",
+                principalTable: "Videos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
