8b4e1acc87398b41eca0789af60ffcac9b1ba9a6
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-19 08:39:01 -0700

    feat(infrastructure): add service registration for infrastructure
    - Created `InfraestructureServiceRegistration` to centralize DI configuration.
    - Added database context registration with SQL Server support.
    - Configured repositories: `BaseRepository`, `VideoRepository`, and `StreamerRepository`.
    - Enabled email services configuration with `EmailSettings`.
    - Updated `GlobalUsings.cs` with new dependencies for infrastructure.

---
+++ CleanArchitecture.Infraestructure/InfraestructureServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿namespace CleanArchitecture.Infraestructure;
+
+public static class InfraestructureServiceRegistration
+{
+    public static IServiceCollection AddInfraestructureServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<StreamerDbContext>(option => 
+            option.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped(typeof(IAsyncRepository<>), typeof(BaseRepository<>));
+        services.AddScoped<IVideoRepository, VideoRepository>();
+        services.AddScoped<IStreamerRepository, StreamerRepository>();
+        
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailService, EmailService>();
+        
+        return services;
+    }
+}
