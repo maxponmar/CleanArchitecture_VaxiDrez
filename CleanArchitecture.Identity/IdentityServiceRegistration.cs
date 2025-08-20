4beaa4f3e753b00c12a527658d682629de6dfc9f
Maximiliano Ponce <maximiliano.ponce@molex.com>
2025-08-20 08:24:18 -0700

    feat(identity): add JWT configuration and service registration
    - Introduced `IdentityServiceRegistration` class for setting up identity services.
    - Configured JWT authentication with issuer, audience, and signing key validation.
    - Added `AddIdentityServices` extension method for seamless service integration.
    - Updated `GlobalUsings.cs` with required namespaces for authentication and DI.

---
+++ CleanArchitecture.Identity/IdentityServiceRegistration.cs
@@ -0,0 +1,42 @@
+﻿namespace CleanArchitecture.Identity;
+
+public static class IdentityServiceRegistration
+{
+    public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        services.AddDbContext<CleanArchitectureIdentityDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("IdentityConnection"),
+                b => b.MigrationsAssembly(typeof(CleanArchitectureIdentityDbContext).Assembly.FullName)));
+
+        services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<CleanArchitectureIdentityDbContext>()
+            .AddDefaultTokenProviders();
+        
+        services.AddTransient<IAuthService, AuthService>();
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            var jwtKey = configuration["JwtSettings:Key"];
+            if(jwtKey == null)
+                throw new Exception("JwtKey is null");
+            
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero,
+                ValidIssuer = configuration["JwtSettings:Issuer"],
+                ValidAudience = configuration["JwtSettings:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+            };
+        });
+        
+        return services;
+    }
+}
